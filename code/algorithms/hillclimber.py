19288eb19dcaa4a7ebdba874d9c5e7b74b6232b3
sedadenboer <sedadenboer21@gmail.com>
2022-01-23 17:23:51 +0100

    pseudocode hillclimber

---
+++ code/algorithms/hillclimber.py
@@ -0,0 +1,11 @@
+from code.classes.traject import Traject
+
+import random
+import copy
+
+# kies een random (geldige) staat:
+# herhaal x iteraties:
+#     maak een kopie van de huidige staat
+#     muteer de kopie
+#     als de staat is verbeterd (K is hoger)
+#         vervang de oude staat door de nieuwe
