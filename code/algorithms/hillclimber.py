38169e00de1bf5d2e15dac709e7f2153ac33df5e
Thijmen Cornelis <thijmen.cornelis@gmail.com>
2022-01-24 17:18:24 +0100

    attempt at continuing hillclimber

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -57,7 +57,32 @@ class Hillclimber:
             new_graph = random_start_state
 
         #     #TODO: muteer de kopie
-        #     #TODO: als de staat is verbeterd (K is hoger):
-        #         # TODO: vervang de oude staat door de nieuwe
+            # remove random traject from lijnvoering
+            random_traject = random.choice(new_graph.lijnvoering.trajecten)
+            new_graph.lijnvoering.remove(random_traject)
+
+            # remove stations and connections of random traject from lijnvoering
+            #TODO: check if stations or connections are not already present in other trajects before removing them from lijnvoering
+            for station in random_traject.stations:
+                new_graph.visited_stations.remove(station)
+            
+            for connection in random_traject.connections:
+                new_graph.used_connections.remove(connection)
+
+            # if not yet all stations and max. number of trajects reached, add new traject to lijnvoering 
+            while help.reached_max_depth(new_graph) == False and help.visited_all_stations(new_graph) == False:
+                help.new_traject(new_graph, start_stations, self.greedy_choice)
+
+            # add quality-goalfunction
+            new_graph.lijnvoering_kwaliteit(new_graph.used_connections, \
+                                            new_graph.available_connections, \
+                                            new_graph.lijnvoering.trajecten)
         
-        self.graph = new_graph
+        #     #TODO: als de staat is verbeterd (K is hoger):
+            #     # TODO: vervang de oude staat door de nieuwe
+            if new_graph.K > opt_K:
+                opt_K = new_graph.K
+                opt_map = new_graph
+
+        
+        self.graph = opt_map
